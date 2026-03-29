afb7765bcd0cc3b2c9dcfc49508f8c88039f14cb
malikharoon-dev <malikharoonahmad191@gmail.com>
2026-03-29 22:37:29 +0500

    Add files via upload

---
+++ README.py
@@ -0,0 +1,17 @@
+# Python Calculator
+
+This is a simple calculator built using Python.
+
+## Features
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+## How to Run
+1. Install Python
+2. Run the file:
+   python calculator.py
+
+## Author
+Malik Haroon Ahmad
